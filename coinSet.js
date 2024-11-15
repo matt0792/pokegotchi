511240c283ec5c43e74876bab83465268c705e38
Matt Thompson <matthew.thompson6@icloud.com>
2024-11-16 01:19:32 +0200

    Add easy pokemon open with spacebar press

--- coinSet.js
+++ coinSet.js
@@ -2,6 +2,7 @@ let coinAmount = JSON.parse(sessionStorage.getItem("userCoins")) || 0;
 sessionStorage.setItem("userCoins", JSON.stringify(coinAmount));
 let coinDisplay = document.getElementById("currencyAmount");
 coinDisplay.textContent = coinAmount;
+let spaceRepeat = document.getElementById("spaceRepeat");
 
 function openPokemon() {
   if (coinAmount >= 20) {
@@ -15,4 +16,15 @@ let circleCostText = document.getElementById("openCost");
 
 if (coinAmount < 20) {
   circleCostText.textContent = "Price: 20";
+}
+
+if (coinAmount >= 40) {
+  spaceRepeat.classList.remove("hidden");
+
+  // Reload on space
+  document.addEventListener("keyup", function (e) {
+    if (e.code === "Space") {
+      window.location.reload();
+    }
+  });
 }
